e3a6a6f7f895cf36b0ff7ea918283a79a47fa52d
naman mohnani <mohnaninaman@gmail.com>
2025-04-20 01:07:44 +0530

    ft. Ponder schema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import { ponder } from "@/generated";
-import { handleV2Factory } from "./handlers/v2factory";
-import { handleV3Factory } from "./handlers/v3factory";
-
-ponder.on("UniswapV2Factory:PairCreated", handleV2Factory);
-ponder.on("UniswapV3Factory:PoolCreated", handleV3Factory);
+console.log("Starting indexer...");
